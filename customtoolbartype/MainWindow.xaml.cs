f13a695c08d50a1519fccaf9056080a1e09ecdec
Faiz Ghazali Desktop <99084931+FaizGhazali@users.noreply.github.com>
2024-11-27 17:58:06 +0800

    DataGrid Manipulation
    Manipulation on DataTemplate

--- customtoolbartype/MainWindow.xaml.cs
+++ customtoolbartype/MainWindow.xaml.cs
@@ -25,32 +25,23 @@ namespace customtoolbartype
         public MainWindow()
         {
             InitializeComponent();
-        }
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23), ImageUrl = "http://www.wpf-tutorial.com/images/misc/john_doe.jpg" });
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
 
-        private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = true;
-        }
-
-        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
+            dgUsers.ItemsSource = users;
         }
     }
 
-    public static class CustomCommands
+    public class User
     {
-        public static readonly RoutedUICommand Exit = new RoutedUICommand
-            (
-                "Exit",
-                "Exit",
-                typeof(CustomCommands),
-                new InputGestureCollection()
-                {
-                    new KeyGesture(Key.F4, ModifierKeys.Alt)
-                }
-            );
+        public int Id { get; set; }
 
-        //Define more commands here, just like the one above
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
