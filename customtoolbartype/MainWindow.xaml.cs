5d261f2ee635d2b328b13f431651ccf637845f4f
Faiz Ghazali Desktop <99084931+FaizGhazali@users.noreply.github.com>
2024-11-27 16:56:20 +0800

    custome command 3
    exit, function untuk refer

--- customtoolbartype/MainWindow.xaml.cs
+++ customtoolbartype/MainWindow.xaml.cs
@@ -27,24 +27,30 @@ namespace customtoolbartype
             InitializeComponent();
         }
 
-        private void CutCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = (txtEditor != null) && (txtEditor.SelectionLength > 0);
+            e.CanExecute = true;
         }
 
-        private void CutCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            txtEditor.Cut();
+            Application.Current.Shutdown();
         }
+    }
 
-        private void PasteCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = Clipboard.ContainsText();
-        }
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand
+            (
+                "Exit",
+                "Exit",
+                typeof(CustomCommands),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(Key.F4, ModifierKeys.Alt)
+                }
+            );
 
-        private void PasteCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            txtEditor.Paste();
-        }
+        //Define more commands here, just like the one above
     }
 }
